686155eb9f86889555fe16833738b9369a7e57df
VadimBarinov <barinovvadim87@gmail.com>
2025-01-12 13:10:43 +0400

    redirect

--- women/urls.py
+++ women/urls.py
@@ -7,8 +7,10 @@ register_converter(converters.FourDigitYearConverter, "year4")
 
 
 urlpatterns = [
-    path("", views.index),      # http://127.0.0.1:8000/
-    path("cats/<int:cat_id>/", views.categories),   # http://127.0.0.1:8000/cats/1/
-    path("cats/<slug:cat_slug>/", views.categories_by_slug),   # http://127.0.0.1:8000/cats/dasda/
-    path("archive/<year4:year>/", views.archive),
+    # лучше всего к маршрутам обращаться по имени (name="..."),
+    # чтобы не было хардкодинга.
+    path("", views.index, name="home"),      # http://127.0.0.1:8000/
+    path("cats/<int:cat_id>/", views.categories, name="cats_id"),   # http://127.0.0.1:8000/cats/1/
+    path("cats/<slug:cat_slug>/", views.categories_by_slug, name="cats"),   # http://127.0.0.1:8000/cats/dasda/
+    path("archive/<year4:year>/", views.archive, name="archive"),
 ]
--- women/views.py
+++ women/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 def index(request):
@@ -18,7 +20,17 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2025:
-        raise Http404()
+        # вычисление URL адреса
+        uri = reverse("cats", args=("sport",))
+
+        # перенаправление на главную страницу
+        # если permanent=True, то код 301 (другой постоянный URL адрес)
+        #      permanent=False(или не указан), то 302 (временный URL адрес)
+        return redirect(uri, permanent=True)
+
+        # второй способ через классы джанго
+        # return HttpResponseRedirect(uri)
+        # return HttpResponsePermanentRedirect(uri)
 
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
