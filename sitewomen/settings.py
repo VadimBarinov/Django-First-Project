d5a5383cf6a9906d3f6aac2e6b18b2b4f356524e
VadimBarinov <barinovvadim87@gmail.com>
2025-01-12 13:26:26 +0400

    templates

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
-
+# здесь нужно прописывать все приложения!!!
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
--- women/urls.py
+++ women/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # лучше всего к маршрутам обращаться по имени (name="..."),
     # чтобы не было хардкодинга.
     path("", views.index, name="home"),      # http://127.0.0.1:8000/
+    path("about/", views.about, name="about"),      # http://127.0.0.1:8000/about/
     path("cats/<int:cat_id>/", views.categories, name="cats_id"),   # http://127.0.0.1:8000/cats/1/
     path("cats/<slug:cat_slug>/", views.categories_by_slug, name="cats"),   # http://127.0.0.1:8000/cats/dasda/
     path("archive/<year4:year>/", views.archive, name="archive"),
