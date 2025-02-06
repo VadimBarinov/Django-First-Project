900040eb9464e0e9845323bbc7498312d1a56449
VadimBarinov <barinovvadim87@gmail.com>
2025-02-06 12:58:19 +0400

    create model

--- women/models.py
+++ women/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Women(models.Model):
+    title = models.CharField(max_length=255)
+    # blank=True можно не передавать значение при создании записи
+    content = models.TextField(blank=True)
+    # auto_now_add=True автозаполнение времени в момент первого появления
+    time_create = models.DateTimeField(auto_now_add=True)
+    # auto_now=True автозаполнение времени при любом изменении записи
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
