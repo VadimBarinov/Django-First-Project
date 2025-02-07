7ba1c8a4b6fdadc907784fe588c7e4321150790c
VadimBarinov <barinovvadim87@gmail.com>
2025-02-07 13:29:50 +0400

    created choices for published in models.py

--- women/models.py
+++ women/models.py
@@ -2,7 +2,18 @@ from django.db import models
 from django.urls import reverse
 
 
+# класс для кастомного менеджера
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
+
+
 class Women(models.Model):
+    # класс для отображения в форме вместо (0/1) будет (черновик/опубликован)
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликован'
+
     title = models.CharField(max_length=255)
     # blank=True можно не передавать значение при создании записи
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
@@ -11,8 +22,11 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     # auto_now=True автозаполнение времени при любом изменении записи
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=Status.DRAFT, choices=Status.choices)
 
+    objects = models.Manager()
+    # новый менеджер
+    published = PublishedManager()
 
     # чтобы отображался только title записи
     def __str__(self):
